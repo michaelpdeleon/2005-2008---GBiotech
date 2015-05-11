a27fddd95a0059b31adfc9398d11576ed23e2b32
Michael de Leon <mike@mikes-air.home>
2015-05-10 20:33:57 -0700

    initial commit

---
+++ js/copyright.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+
+document.write('<p style="margin:0px">Copyright &copy 2008 Genesis Biotechnology Campus, L.L.C.</p>')
---
+++ js/header_subfolder.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+document.write('<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="987" height="183" id="genesislogo08.03.31" align="middle">')
+document.write('<param name="allowScriptAccess" value="sameDomain" />')
+document.write('<param name="movie" value="../images/genesislogo08.03.31.swf" />')
+document.write('<param name="menu" value="false" />')
+document.write('<param name="quality" value="high" />')
+document.write('<param name="bgcolor" value="#ffffff" />')
+document.write('<embed src="../images/genesislogo08.03.31.swf" menu="false" quality="high" bgcolor="#ffffff" width="987" height="183" name="genesislogo08.03.31" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />')
+document.write('</object>')
